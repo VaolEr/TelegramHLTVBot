0640a632e9b66540b833884f9e5f73c30bc20a9f
VaolEr <eremizin@gmail.com>
2021-05-03 11:56:28 +0300

    Init commit

---
+++ src/main/java/com/valoler/telegram_hltv_bot/TelegramHltvBotApplication.java
@@ -0,0 +1,13 @@
+package com.valoler.telegram_hltv_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramHltvBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramHltvBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valoler/telegram_hltv_bot/TelegramHltvBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valoler.telegram_hltv_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramHltvBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
